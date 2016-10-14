300a0ab2fcc114ab8846c924430392e7eaa9f74c
usuario <usuario@10.22.143.41>
2016-10-14 11:15:40 +0100

    Version1

---
+++ src/kata3/Kata3.java
@@ -0,0 +1,11 @@
+package kata3;
+/**
+ * Alberto Hernández Rodríguez Doble Grado
+ */
+public class Kata3 {
+    
+    public static void main(String[] args) {
+        HistogramDisplay histo=new HistogramDisplay();
+        histo.execute();
+    }
+}
